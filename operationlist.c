07e99e6aacf04b7666a7e91d156298ca78a2e7bd
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2023-11-01 19:26:14 +1100

    Add .clang-format Clion formatting has been a bit weird in places and even undoing indent settings so switching to clang formatting.

--- operationlist.c
+++ operationlist.c
@@ -8,23 +8,23 @@
 typedef enum {
   op_halt = 0x0, // halt
   // ARITHMETIC and LOGICAL operations
-  op_addition = 0x1, // add             R[d] <- R[s] +  R[t]
-  op_subtract = 0x2, // subtract        R[d] <- R[s] -  R[t]
+  op_addition = 0x1,   // add             R[d] <- R[s] +  R[t]
+  op_subtract = 0x2,   // subtract        R[d] <- R[s] -  R[t]
   op_bitwiseand = 0x3, // and             R[d] <- R[s] &  R[t]
   op_bitwisexor = 0x4, // xor             R[d] <- R[s] ^  R[t]
-  op_shiftleft = 0x5, // shift left      R[d] <- R[s] << R[t]
+  op_shiftleft = 0x5,  // shift left      R[d] <- R[s] << R[t]
   op_shiftright = 0x6, // shift right     R[d] <- R[s] >> R[t]
   // TRANSFER between registers and memory
-  op_loadimm = 0x7, // load address    R[d] <- addr
-  op_load = 0x8, // load            R[d] <- M[addr]
-  op_store = 0x9, // store           M[addr] <- R[d]
-  op_loadindr = 0xA, // load indirect   R[d] <- M[R[t]]
-  op_storeindr = 0xB, // store indirect  M[R[t]] <- R[d]
+  op_loadimm = 0x7,   // load address     R[d] <- addr
+  op_load = 0x8,      // load             R[d] <- M[addr]
+  op_store = 0x9,     // store            M[addr] <- R[d]
+  op_loadindr = 0xA,  // load indirect    R[d] <- M[R[t]]
+  op_storeindr = 0xB, // store indirect   M[R[t]] <- R[d]
   // CONTROL
   op_branchzero = 0xC, // branch zero     if (R[d] == 0) PC <- addr
-  op_branchpos = 0xD, // branch positive if (R[d] >  0) PC <- addr
-  op_jump = 0xE, // jump register   PC <- R[d]
-  op_call = 0xF  // jump and link   R[d] <- PC; PC <- addr
+  op_branchpos = 0xD,  // branch positive if (R[d] >  0) PC <- addr
+  op_jump = 0xE,       // jump register   PC <- R[d]
+  op_call = 0xF,       // jump and link   R[d] <- PC; PC <- addr
 } op_code;
 
 typedef enum {
@@ -32,7 +32,7 @@ typedef enum {
   format_R__, // Jump(E)
   format_R_R, // Load indirect and Store indirect(A, B)
   format_RRR, // Addition through Shift right(1-6)
-  format_RAA // Load immediate through Store, Branch Zero, Branch Positive and Call(7-9, C, D, F)
+  format_RAA, // Load immediate through Store, Branch Zero, Branch Positive and Call(7-9, C, D, F)
 } op_code_format;
 
 typedef struct {
@@ -46,41 +46,41 @@ typedef struct {
 } op_info;
 
 const op_code_format op_code_format_mapping[] = {
-    format____, // op_halt
-    format_RRR, // op_addition
-    format_RRR, // op_subtract
-    format_RRR, // op_bitwiseand
-    format_RRR, // op_bitwisexor
-    format_RRR, // op_shiftleft
-    format_RRR, // op_shiftright
-    format_RAA, // op_loadimm
-    format_RAA, // op_load
-    format_RAA, // op_store
-    format_R_R, // op_loadindr
-    format_R_R, // op_storeindr
-    format_RAA, // op_branchzero
-    format_RAA, // op_branchpos
-    format_R__, // op_jump
-    format_RAA // op_call
+  format____, // op_halt
+  format_RRR, // op_addition
+  format_RRR, // op_subtract
+  format_RRR, // op_bitwiseand
+  format_RRR, // op_bitwisexor
+  format_RRR, // op_shiftleft
+  format_RRR, // op_shiftright
+  format_RAA, // op_loadimm
+  format_RAA, // op_load
+  format_RAA, // op_store
+  format_R_R, // op_loadindr
+  format_R_R, // op_storeindr
+  format_RAA, // op_branchzero
+  format_RAA, // op_branchpos
+  format_R__, // op_jump
+  format_RAA, // op_call
 };
 
 const c8 *op_format_strings[] = {
   "Halt",
   "Addition, R[%" PRIX8 "] <- R[%" PRIX8 "] + R[%" PRIX8 "]",
-    "Subtraction, R[%" PRIX8 "] <- R[%" PRIX8 "] - R[%" PRIX8 "]",
-    "Bitwise and, R[%" PRIX8 "] <- R[%" PRIX8 "] & R[%" PRIX8 "]",
-    "Bitwise xor, R[%" PRIX8 "] <- R[%" PRIX8 "] ^ R[%" PRIX8 "]",
-    "Shift left, R[%" PRIX8 "] <- R[%" PRIX8 "] << R[%" PRIX8 "]",
-    "Shift right, R[%" PRIX8 "] <- R[%" PRIX8 "] >> R[%" PRIX8 "]",
-    "Load immediate, R[%" PRIX8 "] <- 00%" PRIX8 "%" PRIX8,
-    "Load, R[%" PRIX8 "] <- M[%" PRIX8 "%" PRIX8 "]",
-    "Store, M[%" PRIX8 "%" PRIX8 "] <- R[%" PRIX8 "]",
+  "Subtraction, R[%" PRIX8 "] <- R[%" PRIX8 "] - R[%" PRIX8 "]",
+  "Bitwise and, R[%" PRIX8 "] <- R[%" PRIX8 "] & R[%" PRIX8 "]",
+  "Bitwise xor, R[%" PRIX8 "] <- R[%" PRIX8 "] ^ R[%" PRIX8 "]",
+  "Shift left, R[%" PRIX8 "] <- R[%" PRIX8 "] << R[%" PRIX8 "]",
+  "Shift right, R[%" PRIX8 "] <- R[%" PRIX8 "] >> R[%" PRIX8 "]",
+  "Load immediate, R[%" PRIX8 "] <- 00%" PRIX8 "%" PRIX8,
+  "Load, R[%" PRIX8 "] <- M[%" PRIX8 "%" PRIX8 "]",
+  "Store, M[%" PRIX8 "%" PRIX8 "] <- R[%" PRIX8 "]",
   "Load indirect",
   "Store indirect",
-    "Branch if zero, if (R[%" PRIX8 "] == 0) goto %" PRIX8 "%" PRIX8,
-    "Branch if positive, if (R[%" PRIX8 "] > 0) goto %" PRIX8 "%" PRIX8,
+  "Branch if zero, if (R[%" PRIX8 "] == 0) goto %" PRIX8 "%" PRIX8,
+  "Branch if positive, if (R[%" PRIX8 "] > 0) goto %" PRIX8 "%" PRIX8,
   "Jump",
-    "Call function, R[%" PRIX8 "] <- PC; goto %" PRIX8 "%" PRIX8
+  "Call function, R[%" PRIX8 "] <- PC; goto %" PRIX8 "%" PRIX8,
 };
 
 #define UNKNOWN "Unknown"
@@ -92,7 +92,7 @@ op_info *getopinfo(u16 word) {
     return &info;
   }
 
-  info.op = (op_code) (word >> 12);
+  info.op = (op_code)(word >> 12);
   info.dstOperand = (word >> 8) & 0xF;
   info.src1Operand = (word >> 4) & 0xF;
   info.src2Operand = word & 0xF;
@@ -102,8 +102,8 @@ op_info *getopinfo(u16 word) {
 size getopdesclen(op_info *info) {
   if (info->unknown) {
     return lengthof(UNKNOWN);
-  } else if (op_code_format_mapping[info->op] == format_RRR
-      || (op_code_format_mapping[info->op] == format_RAA && info->op != op_store)) {
+  } else if (op_code_format_mapping[info->op] == format_RRR ||
+             (op_code_format_mapping[info->op] == format_RAA && info->op != op_store)) {
     return snprintf(NULL, 0, op_format_strings[info->op & 0xF], info->dstOperand, info->src1Operand, info->src2Operand);
   } else if (info->op == op_store) {
     return snprintf(NULL, 0, op_format_strings[info->op & 0xF], info->src1Operand, info->src2Operand, info->dstOperand);
@@ -117,20 +117,12 @@ c8 *getopdesc(op_info *info) {
 
   if (info->unknown) {
     snprintf(opdesc, opdesclen, UNKNOWN);
-  } else if (op_code_format_mapping[info->op] == format_RRR
-      || (op_code_format_mapping[info->op] == format_RAA && info->op != op_store)) {
-    snprintf(opdesc,
-             opdesclen,
-             op_format_strings[info->op & 0xF],
-             info->dstOperand,
-             info->src1Operand,
+  } else if (op_code_format_mapping[info->op] == format_RRR ||
+             (op_code_format_mapping[info->op] == format_RAA && info->op != op_store)) {
+    snprintf(opdesc, opdesclen, op_format_strings[info->op & 0xF], info->dstOperand, info->src1Operand,
              info->src2Operand);
   } else if (info->op == op_store) {
-    snprintf(opdesc,
-             opdesclen,
-             op_format_strings[info->op & 0xF],
-             info->src1Operand,
-             info->src2Operand,
+    snprintf(opdesc, opdesclen, op_format_strings[info->op & 0xF], info->src1Operand, info->src2Operand,
              info->dstOperand);
   } else {
     snprintf(opdesc, opdesclen, op_format_strings[info->op & 0xF]);
@@ -144,9 +136,7 @@ int main(void) {
     op_info *info = getopinfo(word);
 
     c8 *infodesc = getopdesc(info);
-    printf("%04"
-    PRIX32
-    ": %s\n", word, infodesc);
+    printf("%04" PRIX32 ": %s\n", word, infodesc);
     free(infodesc);
   }
 
